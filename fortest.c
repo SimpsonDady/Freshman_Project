dfb44777a66ef068d70297d46c1472db7bde2f3b
李香蘭 <evil94710@gmail.com>
2016-12-04 19:51:17 +0800

    move

--- fortest.c
+++ fortest.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int main(){
-    printf("Hello world.");
-    return 0;
+void move (int money[],int location[],int i)
+{
+    srand(time(NULL));
+    int dice1=rand()%6+1,dice2=rand()%6+1,total;
+    total=dice1+dice2;
+    printf("The dice point:%d+%d=%d",dice1,dice2,total);
+    location[i]+=total;
+    if(location[i]>=20){
+        location[i]-=20;
+        if(location[i]==0){
+        }
+        else{
+            money[i]+=2000;
+        }
+    }
 }
